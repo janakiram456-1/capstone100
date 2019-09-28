1845d0d9236db9215affa4db59179c67df342fa1
deepakatreja <datreja@gmail.com>
2019-09-28 05:18:27 -0700

    added commits

--- Calcy.py
+++ Calcy.py
@@ -19,11 +19,14 @@ print("3.Multiply")
 print("4.Divide")
 # Take input from the user
 choice = input("Enter choice(+ | - | / | x):")
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
+num1 = int(input("Enter first number: ")) # take in first number
+num2 = int(input("Enter second number: ")) # take in the second number
+
+# prints the added number
 if choice == '+':
    print(num1,"+",num2,"=", add(num1,num2))
 elif choice == '-':
+# prints the subtracted number
    print(num1,"-",num2,"=", subtract(num1,num2))
 elif choice == 'x':
    print(num1,"*",num2,"=", multiply(num1,num2))
