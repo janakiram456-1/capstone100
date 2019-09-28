f7dbaa47c89ccf28c32ec949e3d694b4f598aa9e
AkshayeK9 <akshaye.khanna@st.niituniversity.in>
2019-09-28 17:17:33 +0530

    AKshaye res

--- Calcy.py
+++ Calcy.py
@@ -4,3 +4,13 @@ def add(x, y):
 def divide(x, y):
    return x / y
 # This function subtracts two numbers
+if choice == '+':
+   print(num1,"+",num2,"=", add(num1,num2))
+elif choice == '-':
+   print(num1,"-",num2,"=", subtract(num1,num2))
+elif choice == 'x':
+   print(num1,"*",num2,"=", multiply(num1,num2))
+elif choice == '/':
+   print(num1,"/",num2,"=", divide(num1,num2))
+else:
+   print("Invalid input")
